52625f8c82ad97247533a8f8df23a424dc117a0f
Alan Dsilva <deathbringer2604@gmail.com>
2020-06-10 01:08:45 +0530

    Create student-group.dart

---
+++ students/student-group.dart
@@ -0,0 +1,16 @@
+[{
+	"id": "1",
+	"name": "Student Name",
+	"email": "Student email",
+	"pid": "111111",
+	"subjectsData": {
+		"Operating System": {
+			"subjectCode": "OS",
+			"attendance": "70"
+		},
+		"Applied Mathematics 3": {
+			"subjectCode": "AM3",
+			"attendance": "45"
+		}
+	}
+}]
